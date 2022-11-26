407b83b791b33f107a4ba06aa886fbaa97993d1f
ovoskresenskyy <7949894@ukr.net>
2022-11-26 03:43:57 +0200

    Added Server Can listen for new connections Collect all connections into the list Send messages to all connected clients

---
+++ src/main/java/Server.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Server {
+
+    private static final String SERVER_NAME = "[SERVER]";
+    private static int sessionNumber;
+    public static List<ConnectedClient> clients = new LinkedList<>();
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(10160)) {
+            System.out.println("Server is started!");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                try {
+                    ConnectedClient client = new ConnectedClient(clientSocket, "Client-"+(++sessionNumber));
+                    clients.add(client);
+
+                    sendAll(client.getName() + " successfully connected.");
+                } catch (IOException e) {
+                    System.out.println("Connection is broken.");
+                    clientSocket.close();
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Connection is broken.");
+        }
+    }
+
+    private static void sendAll(String message) {
+        Server.clients.forEach(client -> client.sendMessage(SERVER_NAME, message));
+    }
+
+
+}

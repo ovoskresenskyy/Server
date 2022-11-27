929f0a9ad9a5d9ea60985dfa81f8fbb394730302
7949894@ukr.net <Fire2740>
2022-11-27 22:01:20 +0200

    New class ClientConnector to communicate with connected clients. Runnable
    used builder to initialize. At that moment creating and starting new Thread.
    
    Menu is not completed

---
+++ src/main/java/org/example/model/ClientConnector.java
@@ -0,0 +1,67 @@
+package org.example.model;
+
+import org.example.NotMyExecutor;
+import org.example.enums.Command;
+import lombok.Data;
+import org.example.service.ClientConnectorService;
+import org.example.service.MenuService;
+
+import java.io.*;
+import java.net.Socket;
+import java.time.LocalDate;
+import java.util.Map;
+
+@Data
+public class ClientConnector implements Runnable {
+
+    private Thread thread;
+    private final Socket socket;
+    private final LocalDate connectionTimeStamp;
+    private final BufferedReader reader;
+    private final BufferedWriter sender;
+
+    public static ClientConnector createAndStart(Socket socket, String name) throws IOException {
+
+        ClientConnector clientConnector = new ClientConnector(socket, name);
+        clientConnector.thread.start();
+        return clientConnector;
+    }
+
+    private ClientConnector(Socket socket, String name) throws IOException {
+
+        thread = new Thread(this, name);
+        this.socket = socket;
+        connectionTimeStamp = LocalDate.now();
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        sender = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    }
+
+    @Override
+    public void run() {
+
+        String userInput;
+
+        ClientConnectorService clientConnectorService = new ClientConnectorService(this);
+        Map<Command, NotMyExecutor> commands = initializeCommands(clientConnectorService);
+        try {
+            do {
+                MenuService.printCommandMenu(this);
+
+                userInput = reader.readLine();
+
+                commands.getOrDefault(Command.getFromString(userInput), clientConnectorService.wrongCommand())
+                        .execute();
+            } while (!userInput.equals("-exit"));
+        } catch (IOException e) {
+            // todo: make own exception
+        }
+    }
+
+    private Map<Command, NotMyExecutor> initializeCommands(ClientConnectorService clientConnectorService) {
+        return Map.of(
+                Command.SEND_FILE, clientConnectorService.sendFile(),
+                Command.MESSAGE, clientConnectorService.sendMessageForAllConnected(),
+                Command.EXIT, clientConnectorService.closeConnection()
+        );
+    }
+}

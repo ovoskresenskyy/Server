c2e54c4d4c6b34cbfa94f8847bc509e475afd847
7949894@ukr.net <Fire2740>
2022-11-28 19:37:40 +0200

    Reworked because it's not abut nullpointer

--- src/main/java/org/example/exceptions/UserInputIsNullException.java
+++ src/main/java/org/example/exceptions/UserInputIsNullException.java
@@ -1,6 +1,6 @@
 package org.example.exceptions;
 
-public class UserInputIsNullException extends NullPointerException {
+public class UserInputIsNullException extends RuntimeException {
 
     public UserInputIsNullException() {
         super("User closed application incorrectly. Command is null.");

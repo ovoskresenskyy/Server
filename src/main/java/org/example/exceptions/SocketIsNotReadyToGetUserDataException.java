f81f1f5cbef73b0daf0adeac4ea667645a7f3264
ovoskresenskiy <7949894@ukr.net>
2022-12-01 14:21:09 +0200

    Deleted exception

--- src/main/java/org/example/exceptions/SocketIsNotReadyToGetUserDataException.java
+++
@@ -1,9 +0,0 @@
-package org.example.exceptions;
-
-public class SocketIsNotReadyToGetUserDataException extends RuntimeException {
-
-    public SocketIsNotReadyToGetUserDataException() {
-        super("Socket is closed or not connected while user input data.");
-    }
-}
-
